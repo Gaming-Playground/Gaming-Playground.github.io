782f646458648c2bc2ae56ba2d8e5f3b28759a28
Steven McDaniel <79287709+StevenMcDaniel@users.noreply.github.com>
2021-06-25 11:03:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-document.getElementsByClassName("nav-btn").onclick = function() {
+var titleTxt = "Gaming Playground";
+var i = 0;
+
+function title() {
+  var currentTxt = document.getElementById("title").innerHTML;
+  currentTxt += titleTxt.charAt(i);
+  if (i < titleTxt.length) {
+    window.setTimeout(title, 250);
+  }
+}
+
+document.getElementsByClassName("nav-btn")[0].onclick = function() {
   window.location = "index.html"
 }
+
+document.getElementsByClassName("nav-btn")[1].onclick = function() {
+  window.location = "games.html"
+}
+
+document.getElementsByClassName("nav-btn")[2].onclick = function() {
+  window.location = "contact.html"
+}
+
+window.setTimeout(title, 1500);
